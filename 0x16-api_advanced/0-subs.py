61eddcee0b1346d96ef5e3ef79ce0a189c81fe9a
Roro1839 <ronaldokipkirui90@gmail.com>
2023-05-09 17:23:30 -0700

    using reddit today

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyBot"}  # Set a custom User-Agent header
+
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    except (requests.exceptions.RequestException, KeyError):
+        return 0
+
